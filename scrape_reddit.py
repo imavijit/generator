13d215fc545da935b2a023148ee3daccd98f851d
APU <avijits545@gmail.com>
2020-01-08 17:57:53 +0530

    first commit

---
+++ scrape_reddit.py
@@ -0,0 +1,13 @@
+import praw
+reddit = praw.Reddit(client_id='QXeIVRnwCi17Tw',
+                     client_secret='Lbo_78FkdTLvoUSfqaAv0_HgzxA',
+                     user_agent='reddit_scrape')
+
+hot_posts = reddit.subreddit('Jokes').hot(limit=10000)
+f= open("jokes.txt","w+", encoding="utf-8")
+i=0
+for post in hot_posts:
+    i=i+1
+    print(post.title)
+    print(i)
+    f.write(post.title +" "+"<|endoftext|>\n")
